686f1d8f7676f3fd0b9630216614d769e3df8cb6
mmmatt <mmmattaus@gmail.com>
2026-03-29 02:28:57 +0800

    Add bootstrap fixed-square scenario scaffold

---
+++ settings.lua
@@ -0,0 +1,11 @@
+data:extend({
+  {
+    type = "int-setting",
+    name = "fes-starting-square-size",
+    setting_type = "startup",
+    default_value = 7,
+    minimum_value = 3,
+    maximum_value = 255,
+    order = "a"
+  }
+})
